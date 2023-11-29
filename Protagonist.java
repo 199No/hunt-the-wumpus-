c741f4c8d883d7a017274d62698310489eaea7c4
Noah Wichman <139374352+199No@users.noreply.github.com>
2023-11-29 12:54:27 -0800

    Update Protagonist.java

--- Protagonist.java
+++ Protagonist.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+class Protagonist{
+  Scanner user;
+  int attack;
+  int health;
+  String choose;
+  int heal;
+  public Protagonist(int attack, int health, Scanner user){
+    this.attack = attack;
+    this.health = health;
+    this.user = user;
+    this.heal = 10;
+  }
+
+  public String choice(){
+    System.out.println("Would you like to attack, heal or run?");
+    System.out.println("______________________________________");
+    System.out.println();
+    choose = user.next();
+    return choose;
+  }
+}
